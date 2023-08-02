0cf6b467e4f8329cb6303ce1d7fcf6fee8acc532
AndrewLiao <andrew@weibyapps.com>
2023-08-02 17:48:47 +0800

    add more status

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
+
 	ec := ecpayShipping.NewEcpayShipping(ecpayShipping.EcpayConfig{
 		MerchantID:   "2000933",
 		HashKey:      "XBERn1YOvpM9nfZc",
 		HashIV:       "h1ONHk4P4yqbl5LK",
 		IsProduction: false,
 
-		SenderName:     "測試人員",
-		SenderPhone:    "0912345678",
-		ServerReplyURL: "https://www.ecpay.com.tw/receive.php",
+		SenderName:     "test",
+		SenderPhone:    "912345678",
+		ServerReplyURL: "http://localhost:8000/test",
 	})
+
 	// resp, err := ec.ChooseShipStore(ecpayShipping.ChooseShipStoreConfig{
 	// 	MerchantTradeNo:   "test123456789",
 	// 	ShippingStoreType: "FAMI",
@@ -26,15 +28,15 @@ func main() {
 	// 	IsMobile:          false,
 	// })
 	resp, err := ec.CreateShipOrder(ecpayShipping.CreateShippingOrderConfig{
-		MerchantTradeNo:   "test1237a",
+		MerchantTradeNo:   "4b4797b817",
 		TradeDate:         time.Now(),
 		ShippingStoreType: "711",
-		Amount:            100,
-		NeedPayment:       false,
-		EntreeName:        "測試物品",
-		ReceiverName:      "測試人員",
+		Amount:            150,
+		NeedPayment:       true,
+		EntreeName:        "name",
+		ReceiverName:      "你好嗎",
 		ReceiverPhone:     "0912345678",
-		ReceiverEmail:     "abc@gmail.com",
+		ReceiverEmail:     "test@gmail.com",
 		ReceiverStoreID:   "131386",
 		ClientReplyURL:    "",
 	})
