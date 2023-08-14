7e8670f776f6324b786883802ab2c0423bb27091
AndrewLiao <andrew@weibyapps.com>
2023-08-14 12:00:10 +0800

    add extra for choose store

--- ecpay.go
+++ ecpay.go
@@ -29,6 +29,7 @@ type ChooseShipStoreConfig struct {
 	NeedPayment       bool
 	ServerReplyURL    string
 	IsMobile          bool
+	Extra             interface{}
 }
 
 type ChooseShipStoreResponse struct {
@@ -103,7 +104,7 @@ func (e *EcpayShippingImpl) ChooseShipStore(config ChooseShipStoreConfig) (strin
 		"LogisticsSubType": FormatStoreType(config.ShippingStoreType),
 		"IsCollection":     FormatNeedPayment(config.NeedPayment),
 		"ServerReplyURL":   config.ServerReplyURL,
-		"ExtraData":        "",
+		"ExtraData":        fmt.Sprintf("%v", config.Extra),
 		"Device":           FormatIsMobile(config.IsMobile),
 	}
 
