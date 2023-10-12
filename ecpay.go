2abf05fb525d16edf7964964c6f4df6ae258879f
AndrewLiao <andrew@weibyapps.com>
2023-10-12 14:10:20 +0800

    fix error condition

--- ecpay.go
+++ ecpay.go
@@ -481,7 +481,7 @@ func (e *EcpayImpl) RefundPayment(config RefundConfig) (*RefundResponse, error) 
 		return nil, err
 	}
 
-	if code, ok := result["RtnCode"].(string); !ok || (ok && code != "1") {
+	if _, ok := result["RtnCode"].(string); ok {
 		return nil, fmt.Errorf("query credit card payment error: %v", result["RtnMsg"].(string))
 	}
 
